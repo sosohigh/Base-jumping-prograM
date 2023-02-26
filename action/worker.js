24502d6326746ce7768a703b5060128acd012162
sosohigh <122018780+sosohigh@users.noreply.github.com>
2023-02-26 10:04:11 +0000

    Update résumé.css

---
+++ action/worker.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const pino = require('../pino.js')
+const build = require('pino-abstract-transport')
+const loadTransportStreamBuilder = require('./transport-stream')
+
+// This file is not checked by the code coverage tool,
+// as it is not reliable.
+
+/* istanbul ignore file */
+
+modul
